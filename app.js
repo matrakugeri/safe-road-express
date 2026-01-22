f5e639a1011f751e7b2d7e8ce7e3fe905913b1d7
matrakugeri <matrakugeri@gmail.com>
2026-01-22 22:46:52 +0100

    implement helping functions

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ import express from "express";
 import cors from "cors";
 const app = express();
 import userRouter from "./routes/userRoutes.js";
+import globalErrorHandler from "./controllers/errorController.js";
 
 app.use(express.json());
 app.use(cors());
 
 app.use("/api/v1/users", userRouter);
+
+app.use(globalErrorHandler);
 
 export default app;
