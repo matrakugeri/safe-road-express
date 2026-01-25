fc561956ecfb03ef34d9e46a497409824ee1d4b6
matrakugeri <matrakugeri@gmail.com>
2026-01-25 23:27:07 +0100

    added cookie parser

--- app.js
+++ app.js
@@ -3,9 +3,16 @@ import cors from "cors";
 const app = express();
 import userRouter from "./routes/userRoutes.js";
 import globalErrorHandler from "./controllers/errorController.js";
+import cookieParser from "cookie-parser";
 
+app.use(cookieParser());
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:4200",
+    credentials: true,
+  }),
+);
 
 app.use("/api/v1/users", userRouter);
 
